f8984a42fe3099a60b46baaa7ce706385d09f12a
taiki <buena9926@gmail.comi>
2019-02-25 04:02:20 +0900

    不要コードの削除／.gitignore追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,6 @@
 class Blog < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-	has_many :tags
+	has_many :tags, dependent: :destroy
 end
