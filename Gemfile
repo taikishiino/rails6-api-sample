e8e7b9aeb21c40a0e1a03a88a421221e2a8eeb43
taiki_shiino <buena9926@gmail.com>
2019-03-15 23:45:41 +0900

    FactoryGirlからFactoryBotへ移行

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   gem 'rspec-rails', '~> 3.8.2'
   gem 'spring-commands-rspec'
-  gem 'factory_girl_rails'
+  gem 'factory_bot_rails'
   gem 'shoulda-matchers'
 
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe Article, type: :model do
   describe 'relation' do
     it { is_expected.to have_many(:tags) }
   end
+
+=begin
+  describe "hogehoge" do
+    context "ArticleのFactoryを作成した場合" do
+      it "テストデータが作成される" do
+        article_model = FactoryGirl.create(:article)
+
+        p article_model
+      end
+    end
+  end
+=end
 end
