f93a4b9e75531e4225f13ee4d384a4b630703779
taiki_shiino <buena9926@gmail.com>
2019-03-17 23:51:16 +0900

    graphqlでarticleのCRUD実装

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,13 +1,34 @@
 module Types
   class MutationType < Types::BaseObject
-    field :createBlogMutation, mutation: Mutations::CreateBlogMutation
-    field :updateBlogMutation, mutation: Mutations::UpdateBlogMutation
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    #--------------------------
+    # Article
+    #--------------------------
+    field :create_article, ArticleType, null: false, camelize: false, description: 'Create an article' do
+      argument :title, String, required: true
+      argument :text, String, required: true
+      argument :like_count, Int, required: false
+    end
+    def create_article(title:, text:)
+      Article.create!(title: title, text: text)
+    end
+    # 返り値: Boolean
+    field :update_article, Boolean, null: false, camelize: false, description: 'Delete an article' do
+      argument :id, ID, required: true
+      argument :title, String, required: true
+      argument :text, String, required: true
+    end
+    def update_article(id:, title:, text:)
+      article = Article.find_by!(id: id)
+      article.title = title
+      article.text = text
+      article.save!
+    end
+    field :delete_article, Boolean, null: false, camelize: false, description: 'Delete an article' do
+      argument :id, ID, required: true
+    end
+    def delete_article(id:)
+      Article.find_by!(id: id).destroy!
+      true
     end
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,30 +1,22 @@
 module Types
   class QueryType < Types::BaseObject
-
-    field :test_field, String, null: false,
-      description: "An example field added by the generator", camelize: false
+    field :test_field, String, null: false, camelize: false,
+      description: "An example field added by the generator"
     def test_field
-      "Hello!!!!!!!!"
+      "現在時刻は#{context[:time]}です！"
     end
 
-    # 引数のIDをそのまま返すAPI
-    field :blog_field, ID, null: false,
-          # camelize: false で全てスネークケースでリクエストが送れるようになる
-          description: '店舗をidで検索', camelize: false do
-      # 特定のIDのデータだけ取得したい場合は、argumentで引数を定義する
+    # Article
+    field :articles, [Types::ArticleType], null: false, description: 'returns all articles'
+    def articles
+      Article.all
+    end
+
+    field :article, Types::ArticleType, null: false, description: 'returns a article' do
       argument :id, ID, required: true
     end
-    def blog_field(id:)
-      "BlogID: #{id} を取得！"
-    end
-
-    # blog
-    field :blog, BlogType, null: true do
-      description 'ブログをidで検索'
-      argument :id, ID, 'ブログID', required: false # id がなくても検索できるように
-    end
-    def blog(id: nil)
-      id ? Blog.find(id) : Blog.last
+    def article(id:)
+      Article.find_by(id: id)
     end
   end
 end
