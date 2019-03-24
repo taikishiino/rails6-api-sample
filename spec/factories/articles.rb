e62e5046dc2f86bcfa6fe4fb4630e94b8b5cc24b
taiki_shiino <buena9926@gmail.com>
2019-03-24 18:55:09 +0900

    FactoryBotのデータの生成を重複しない仕様に変更

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :article do
-    title { 'titleタイトルたいとる' }
-    text { 'textテキストてきすと' }
+    sequence(:title) {|n| "blog_title_#{n}"}
+    sequence(:text) {|n| "blog_body_#{n}"}
   end
 end
