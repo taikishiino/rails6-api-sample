faef4cd7184cc3195a683c136b74736634e79215
taikishiino <taiki.shiino@gmail.com>
2019-05-18 11:09:38 +0000

    chenge endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
+  post "/api/v1/graphql", to: "graphql#execute"
 
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
